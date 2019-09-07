72a81264f0ee021c5053513bee0ef5529d9256e5
Olly Maunder <olly@roomaroo.co.uk>
2019-09-07 13:46:28 +0100

    Added timer

--- FabulousDemo/FabulousDemo/FabulousDemo.fs
+++ FabulousDemo/FabulousDemo/FabulousDemo.fs
@@ -6,24 +6,36 @@ open Fabulous.XamarinForms.LiveUpdate
 open Xamarin.Forms
 
 module App = 
-    type Model = { Count : int; StepSize : int }
+    type Model = { Count : int; StepSize : int; TimerOn : bool }
 
     type Msg = 
         | Increment 
         | Decrement 
         | Reset
         | SetStep of int
+        | SetTimer of bool
+        | TimerElapsed
 
+    let initialModel = { Count = 0; StepSize = 1; TimerOn = false }
 
-    let initialModel = { Count = 0; StepSize = 1 }
     let init () = initialModel, Cmd.none
 
+    let timerCmd = 
+        async {
+            do! Async.Sleep 1000
+            return TimerElapsed
+        } |> Cmd.ofAsyncMsg
     let update msg model =
         match msg with
         | Increment -> { model with Count = model.Count + model.StepSize }, Cmd.none
         | Decrement -> { model with Count = model.Count - model.StepSize }, Cmd.none
         | Reset -> initialModel, Cmd.none
         | SetStep n -> { model with StepSize = n}, Cmd.none
+        | SetTimer on -> { model with TimerOn = on }, if on then timerCmd else Cmd.none
+        | TimerElapsed ->
+            {model with Count = model.Count + model.StepSize},
+            if model.TimerOn then timerCmd else Cmd.none
+                
 
     let view (model: Model) dispatch =
         View.ContentPage(
@@ -62,6 +74,18 @@ module App =
                     text = "Reset", 
                     fontSize = 30,
                     command = (fun () -> dispatch Reset))
+
+                View.StackLayout(
+                    padding = 20.0,
+                    scale = 2.0,
+                    horizontalOptions = LayoutOptions.CenterAndExpand,
+                    orientation = StackOrientation.Horizontal,
+                    children = [
+                        View.Label(text = "Timer:")
+                        View.Switch(
+                            isToggled = model.TimerOn,
+                            toggled = (fun args -> dispatch (SetTimer args.Value)))
+                        ])
             ]))
 
     // Note, this declaration is needed if you enable LiveUpdate
