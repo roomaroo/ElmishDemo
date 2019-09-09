83f967d466cc93d764cb3ae05d48b53b849b6a32
Olly Maunder <olly@roomaroo.co.uk>
2019-09-09 23:20:45 +0100

    Using TypedCss

--- FableDemo/src/App.fs
+++ FableDemo/src/App.fs
@@ -21,28 +21,38 @@ let update msg model : Model * Cmd<Msg> =
   | Decrement -> { model with Count = model.Count - 1}, Cmd.none
   | Reset -> initialModel, Cmd.none
 
+
+open Zanaptak.TypedCssClasses
+
+type Bulma = CssClasses<
+              "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css",
+              Naming.PascalCase>
+
 let view model dispatch =
   div
-    [classList [("section", true); ("has-text-centered", true)]]
+    [classList [(Bulma.Section, true); (Bulma.HasTextCentered, true)]]
     [
         h1 
-          []
+          [ClassName 
+            (match model.Count with
+             | n when n < 0 -> Bulma.HasTextDanger
+             | _ -> Bulma.HasTextInfo)]
           [str (sprintf "Counter value: %i" model.Count)]
         
         div 
-          []
+          [ClassName Bulma.Buttons]
           [
             button 
-              [ClassName "button"; OnClick (fun _ -> dispatch Decrement)] 
+              [ClassName Bulma.Button; OnClick (fun _ -> dispatch Decrement)] 
               [str "Decrement"]
             
             button 
-              [ClassName "button"; OnClick (fun _ -> dispatch Increment)] 
+              [ClassName Bulma.Button; OnClick (fun _ -> dispatch Increment)] 
               [str "Increment"]
             
             button 
               [
-                classList [("button", true); ("is-danger", true)];
+                classList [(Bulma.Button, true); (Bulma.IsPrimary, true)];
                 OnClick (fun _ -> dispatch Reset)] 
               [str "Reset"]
           ]
